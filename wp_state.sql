55eca9897a1a8ac692c85b124703d8338bc3f428
Christopher Rabe <blueAlien86@gmail.com>
2013-11-11 11:52:18 -0600

    Added State table

---
+++ wp_state.sql
@@ -0,0 +1,58 @@
+CREATE TABLE IF NOT EXISTS `wp_state` (
+  `state_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `state_name` varchar(255) NOT NULL,
+  `short_name` varchar(5) NOT NULL,
+  PRIMARY KEY (`state_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=52 ;
+
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(1, 'Alaska', 'AK');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(2, 'Alabama', 'AL');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(3, 'Arkansas', 'AR');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(4, 'Arizona', 'AZ');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(5, 'California', 'CA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(6, 'Colorado', 'CO');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(7, 'Connecticut', 'CT');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(8, 'Washington DC', 'WA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(9, 'Delaware', 'DE');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(10, 'Florida', 'FL');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(11, 'Georgia', 'GA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(12, 'Hawaii', 'HI');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(13, 'Iowa', 'IA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(14, 'Idaho', 'ID');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(15, 'Illinois', 'IL');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(16, 'Indiana', 'IN');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(17, 'Kansas', 'KS');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(18, 'Kentucky', 'KY');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(19, 'Louisiana', 'LA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(20, 'Massachusetts', 'MA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(21, 'Maryland', 'MD');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(22, 'Maine', 'ME');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(23, 'Michigan', 'MI');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(24, 'Minnesota', 'MN');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(25, 'Missouri', 'MO');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(26, 'Mississippi', 'MS');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(27, 'Montana', 'MT');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(28, 'North Carolina', 'NC');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(29, 'North Dakota', 'ND');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(30, 'Nebraska', 'NE');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(31, 'New Hampshire', 'NH');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(32, 'New Jersey', 'NJ');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(33, 'New Mexico', 'NM');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(34, 'Nevada', 'NV');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(35, 'New York', 'NY');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(36, 'Ohio', 'OH');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(37, 'Oklahoma', 'OK');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(38, 'Oregon', 'OR');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(39, 'Pennsylvania', 'PA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(40, 'Rhode Island', 'RI');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(41, 'South Carolina', 'SC');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(42, 'South Dakota', 'SD');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(43, 'Tennessee', 'TN');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(44, 'Texas', 'TX');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(45, 'Utah', 'UT');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(46, 'Virginia', 'VA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(47, 'Vermont', 'VT');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(48, 'Washington', 'WA');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(49, 'Wisconsin', 'WI');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(50, 'West Virginia', 'WV');
+INSERT INTO `wp_state` (`state_id`, `state_name`, `short_name`) VALUES(51, 'Wyoming', 'WY');
